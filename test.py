0a137e075bc6a18f164528ee02c3a798e28c7ff2
Med <mederis.frambourt@gmail.com>
2024-09-26 19:22:11 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client
